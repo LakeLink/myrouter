c73b230930df3749c950a792fc838093907ac1b2
Yiffyi Jia <git@yif.fyi>
2025-04-25 21:17:14 +0800

    fix: tproxy failed to route to local due to priority

--- demo-nx30pro/reload-iprule.sh
+++ demo-nx30pro/reload-iprule.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ip rule add fwmark 1100 table 1100 pref 36000
+ip rule add fwmark 1100 table 1100 pref 30100
 ip route add local default dev lo table 1100
 
 # ip -6 rule add to 240c:c7b9:0:400::/64 table main pref 30000
-ip -6 rule add fwmark 1100 table 1100 pref 30010
+ip -6 rule add fwmark 1100 table 1100 pref 30100
 ip -6 route add local default dev lo table 1100
