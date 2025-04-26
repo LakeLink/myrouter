faf26dad797b37f0c108f28a45261d49029687bb
Yiffyi Jia <git@yif.fyi>
2025-04-26 15:55:36 +0800

    add: fix sing-box config check

--- common/sing-box.sh
+++ common/sing-box.sh
@@ -7,7 +7,7 @@ case $1 in
      -c sing-box.base.json \
      -c sing-box.secrets.json
 
-   sing-box check -c sing-box.json -D /root/myrouter
+   sing-box check -c /var/run/sing-box.json || exit 1
 
    killall -9 sing-box
    tmux new-session -d -s "sing-box" "sing-box -c /var/run/sing-box.json run"
