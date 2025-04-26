12a29db775c20852cadd0ad9d5f8ea94ad579c17
Yiffyi Jia <git@yif.fyi>
2025-04-26 14:46:50 +0800

    add: update configs for nx30pro

--- demo-nx30pro/sing-box.sh
+++ demo-nx30pro/sing-box.sh
@@ -2,13 +2,15 @@
 
 case $1 in
  start)
-   sing-box merge sing-box.json \
+   sing-box merge /var/run/sing-box.json \
      -D /root/myrouter \
      -c sing-box.base.json \
      -c sing-box.secrets.json
 
+   sing-box check -c sing-box.json -D /root/myrouter
+
    killall -9 sing-box
-   tmux new-session -d -s "sing-box" "sing-box -c /root/myrouter/sing-box.json run"
+   tmux new-session -d -s "sing-box" "sing-box -c /var/run/sing-box.json run"
    echo `tmux list-panes -F '#{pane_pid}' -t sing-box` > /var/run/myrouter-sing-box.pid ;
    ;;
  stop)
