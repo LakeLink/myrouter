d69b7738d98db5d62a8c1f55dd602bffec60296e
Yiffyi Jia <git@yif.fyi>
2025-05-05 15:57:25 +0800

    add: let's share monitrc

---
+++ common/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd /root/myrouter
+chmod +x *.sh
+
+ln -s /root/myrouter/monitrc-common /etc/monitrc
+chmod 600 /etc/monitrc
+
+mkdir -p /etc/monit.d
+ln -s /root/myrouter/monitrc-myrouter /etc/monit.d/10-myrouter
+
+/etc/init.d/monit restart
+
