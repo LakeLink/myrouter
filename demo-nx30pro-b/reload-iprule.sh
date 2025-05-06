08a5a4585d46265db1aca80b3ea5a73543c68e0f
Yiffyi Jia <git@yif.fyi>
2025-05-06 13:26:21 +0800

    add: nx30pro-b

---
+++ demo-nx30pro-b/reload-iprule.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ip rule add fwmark 1100 table 1100 pref 30100
+ip route add local default dev lo table 1100
+
+# ip -6 rule add to 240c:c7b9:0:400::/64 table main pref 30000
+ip -6 rule add fwmark 1100 table 1100 pref 30100
+ip -6 route add local default dev lo table 1100
