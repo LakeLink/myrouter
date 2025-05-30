8fe7e590fb0d14ca3ddec5fe517278904f847a1f
Yiffyi Jia <git@yif.fyi>
2025-05-31 01:26:13 +0800

    fix: allow ntp to resolve address of servers

--- common/sing-box.sh
+++ common/sing-box.sh
@@ -9,12 +9,12 @@ case $1 in
      -c sing-box.secrets.json
 
    sing-box check -c /var/run/sing-box.json || exit 1
-
-   ntpd -dnq -p ntp.sjtu.edu.cn -p time.apple.com -p time.windows.com -p time.cloudflare.com > /var/log/myrouter-ntp.log 2>&1
-
    killall -9 sing-box
    tmux new-session -d -s "sing-box" "sing-box -c /var/run/sing-box.json run"
    echo `tmux list-panes -F '#{pane_pid}' -t sing-box` > /var/run/myrouter-sing-box.pid ;
+
+   # You might need sing-box up and running to resolve hostname
+   ntpd -dnq -p ntp.sjtu.edu.cn -p time.apple.com -p time.windows.com -p time.cloudflare.com > /var/log/myrouter-ntp.log 2>&1
    ;;
  stop)
    kill -9 `cat /var/run/myrouter-sing-box.pid`
