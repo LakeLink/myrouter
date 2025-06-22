589590fd09de108a407a20ab737ca2198d2a4a07
Yiffyi Jia <git@yif.fyi>
2025-06-22 12:09:51 +0800

    fix: update netdata whitelist ip

--- common/install.sh
+++ common/install.sh
@@ -8,7 +8,12 @@ ln -s /root/myrouter/monitrc-common /etc/monitrc
 chmod 600 /etc/monitrc
 
 mkdir -p /etc/monit.d
+rm /etc/monit.d/10-myrouter
 ln -s /root/myrouter/monitrc-myrouter /etc/monit.d/10-myrouter
 
 /etc/init.d/monit restart
 
+rm /etc/netdata/netdata.conf
+ln -s /root/myrouter/netdata.conf /etc/netdata/netdata.conf
+
+/etc/init.d/netdata restart
