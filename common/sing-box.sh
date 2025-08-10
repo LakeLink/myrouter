ff449baaf30bd798c28292b6565f620e4adbdab3
Yiffyi Jia <git@yif.fyi>
2025-08-10 20:26:48 +0800

    fix: add memory limit on sing-box, better urltest and failsafe

--- common/sing-box.sh
+++ common/sing-box.sh
@@ -9,15 +9,20 @@ case $1 in
      -c sing-box.secrets.json || exit 1
 
    sing-box check -c /var/run/sing-box.json || exit 2
-   killall -9 sing-box
-   tmux new-session -d -s "sing-box" "sing-box -c /var/run/sing-box.json run"
-   echo `tmux list-panes -F '#{pane_pid}' -t sing-box` > /var/run/myrouter-sing-box.pid ;
+
+   # memory limit: 100MB
+   tmux new-session -d -s sing-box -e "GOMEMLIMIT=100000000" "sing-box -c /var/run/sing-box.json run"\; \
+        set-option remain-on-exit failed
+
+   tmux list-panes -F '#{pane_pid}' -t sing-box > /var/run/myrouter-sing-box.pid ;
 
    # You might need sing-box up and running to resolve hostname
    ntpd -dnq -p time.apple.com -p time.windows.com -p ntp1.aliyun.com > /var/log/myrouter-ntp.log 2>&1
    ;;
  stop)
-   kill -9 `cat /var/run/myrouter-sing-box.pid`
+   tmux capture-pane -p -t sing-box > /var/log/myrouter-sing-box-tail.log
+   kill -9 `cat /var/run/myrouter-sing-box.pid` && rm /var/run/myrouter-sing-box.pid
+   tmux kill-session -t "sing-box"
    ;;
  *)
    echo "usage: sing-box {start|stop}" ;;
