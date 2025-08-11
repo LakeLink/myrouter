af858ca435223c426037e18c7809791b9732303e
Yiffyi Jia <git@yif.fyi>
2025-08-12 01:10:53 +0800

    add: update ip rule, dns, firewall

--- yisuo-bio-1/reload-iprule.sh
+++ yisuo-bio-1/reload-iprule.sh
@@ -4,15 +4,17 @@ ip rule add fwmark 1100 table 1100 pref 30100
 ip route add local default dev lo table 1100
 
 # process other routes, only hold default
-ip rule add from all table main suppress_prefixlength 0 pref 30000
+# already added in luci
+# ip rule add from all table main suppress_prefixlength 0 pref 30000
+
 # go through our default routes
 ip rule add fwmark 1000 table 1000 pref 30100
 ip rule add fwmark 1001 table 1001 pref 30100
-ip rule add fwmark 1002 table 1002 pref 30100
-ip rule add fwmark 1003 table 1003 pref 30100
-ip rule add fwmark 1004 table 1004 pref 30100
-ip rule add fwmark 1005 table 1005 pref 30100
-ip rule add fwmark 1006 table 1006 pref 30100
+# ip rule add fwmark 1002 table 1002 pref 30100
+# ip rule add fwmark 1003 table 1003 pref 30100
+# ip rule add fwmark 1004 table 1004 pref 30100
+# ip rule add fwmark 1005 table 1005 pref 30100
+# ip rule add fwmark 1006 table 1006 pref 30100
 # kernel default rule
 # 32766:  from all lookup main
 
