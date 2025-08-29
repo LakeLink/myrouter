b6a5abda74b07101bb4bd1ace264410cd706fe38
Yiffyi Jia <git@yif.fyi>
2025-08-29 13:28:18 +0800

    maybe this works

--- yiffyi-ax6s/reload-iprule.sh
+++ yiffyi-ax6s/reload-iprule.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-# 998 is used for generic output, used to force reroute
-ip rule add fwmark 998 table 998 pref 9000
-ip route add local default dev lo table 998
-
-# 999 is used for sing-box output, will be reset in nftable
-ip rule add fwmark 1000 table 1000 pref 35000
-ip rule add fwmark 1001 table 1001 pref 35000
-ip rule add fwmark 1002 table 1002 pref 35000
-ip rule add fwmark 1003 table 1003 pref 35000
-ip rule add fwmark 1004 table 1004 pref 35000
-
-ip rule add fwmark 1100 table 1100 pref 36000
+ip rule add fwmark 1100 table 1100 pref 30100
 ip route add local default dev lo table 1100
 
-ip -6 rule add to 240c:c7b9:0:400::/64 table main pref 30000
-ip -6 rule add fwmark 1100 table 1100 pref 30010
+# process other routes, only hold default
+ip rule add from all table main suppress_prefixlength 0 pref 30000
+# go through our default routes
+ip rule add fwmark 1000 table 1000 pref 30100
+ip rule add fwmark 1001 table 1001 pref 30100
+ip rule add fwmark 1002 table 1002 pref 30100
+ip rule add fwmark 1003 table 1003 pref 30100
+ip rule add fwmark 1004 table 1004 pref 30100
+# kernel default rule
+# 32766:  from all lookup main
+ip rule add from all table 1000 pref 33000
+
+ip -6 rule add from all table main suppress_prefixlength 0 pref 30000
+ip -6 rule add to 2001:250:6413::/48 table main pref 30010
+ip -6 rule add to 240c:c7b9:0:400::/64 table main pref 30010
+
+ip -6 rule add fwmark 1100 table 1100 pref 30100
 ip -6 route add local default dev lo table 1100
